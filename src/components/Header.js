87acc9eb770dc91202e9d74d50495b4c62938609
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2023-02-09 16:09:32 +0300

    REACT_APP_APP_NAME

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">{process.env.APP_NAME}</Navbar.Brand>
+        <Navbar.Brand href="#home">{process.env.REACT_APP_APP_NAME}</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
